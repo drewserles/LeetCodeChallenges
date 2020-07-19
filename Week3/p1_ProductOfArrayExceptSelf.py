1224c4c474ffc6f9d6d5e4349d0ff1bff46e15e2
Drew Serles <drewserles@gmail.com>
2020-07-19 17:47:52 -0400

    Added low memory solution

--- Week3/p1_ProductOfArrayExceptSelf.py
+++ Week3/p1_ProductOfArrayExceptSelf.py
@@ -13,14 +13,27 @@ class Solution:
         for i in range(sz):
             output[i] = up[i]*down[i]
         return output
+    def pes_space(self, nums):
+        # Only allowed to use memory for output array
+        sz = len(nums)
+        output = sz*[1]
+        tr = 1
+        for i in range(1, sz):
+            tr *= nums[i-1]
+            output[i] *= tr
+        tr = 1
+        for i in range(sz-2,-1,-1):
+            tr *= nums[i+1]
+            output[i] *= tr
+        return output
 
 
 
 if __name__ == "__main__":
     sol = Solution()
-    # nums = [1,2,3,4]
-    nums=[1,0]
-    print(sol.productExceptSelf(nums))
+    nums = [1,2,3,4]
+    # nums=[1,0]
+    print(sol.pes_space(nums))
 
 # Breaks my mult total idea:
 # Input:
