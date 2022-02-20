de086a21be52295ab595c8dede18facaa0a51868
Drew Serles <drewserles@gmail.com>
2022-02-20 10:09:11 -0500

    covered intervals

--- 1000+/p1288.py
+++ 1000+/p1288.py
@@ -1,9 +1,30 @@
 class Solution:
-    def function_name(self):
-        return 0
+    def removeCoveredIntervals(self, intervals):
+        intervals.sort() # Sorts by first key then second key
+        ints = 0
+        ci = intervals[0]
+        for int in intervals[1:]:
+            '''
+            A couple situations to address:
+            -If the interval start of int is the same as ci then int's range is >= ci's since we're sorting.
+                Then int's range will encompass ci so update ci to be int
+            -If interval start is > ci's then two possible things to do:
+                if int's end is <= ci's end then it's fully encompassed. Don't update ci and continue
+                if int's end is > ci's end then it's a on-overlapping interval. Add ci to res, reset ci to int
+            '''
+            if int[0] == ci[0]:
+                ci = int
+            # Other option is it's larger since intervals was sorted
+            else:
+                if int[1] > ci[1]:
+                    ints += 1
+                    ci = int
 
+        ints += 1
+        return ints
 
 if __name__ == "__main__":
     sol = Solution()
-    
-    print(sol.function_name())
+    # intervals = [[1,4],[3,6],[2,8]]
+    intervals = [[1,4], [2,8], [3,6]]
+    print(sol.removeCoveredIntervals(intervals))
