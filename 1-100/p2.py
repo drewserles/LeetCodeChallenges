e685a14d788b6dd64cb125920a12ff03ca79b88f
drewserles <drewserles@gmail.com>
2022-02-28 20:53:26 -0500

    p2 modify in place

--- 1-100/p2.py
+++ 1-100/p2.py
@@ -12,7 +12,8 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def addTwoNumbers(self, l1, l2):
+    # Creates a new node to combine L1 and L2 values
+    def addTwoNumbers_v1(self, l1, l2):
         carry = 0
         head, p = None, None
         while l1 or l2 or carry>0:
@@ -31,6 +32,44 @@ class Solution:
                 p.next = new
                 p = p.next
             carry = next_val // 10
+        return head
+
+    '''
+    Modifies L1 in place.
+    -Keep a pointer at the start of L1 that will be returned at the end.
+    -At each step, replace the value of the L1 node with l1.val + l2.val + carry
+    -If L2 has gotten to its end then just ignore it going forward. No value to add to the sum
+    -If L1 has gotten to its end then two options:
+        -If L2 is also done then create another node with the carry (if there is one) or end if carry is 0
+        -If L2 is not done then point L1 to L2
+    '''
+    def addTwoNumbers(self, l1, l2):
+        carry, head, single = 0, l1, False
+
+        while 1:
+            val = carry + l1.val
+
+            if l2 and not single:
+                val += l2.val
+                l2 = l2.next
+                
+            l1.val = val % 10
+            carry = val // 10
+
+            # The L1 train is ending. handle a couple situations. Not l2 was already incremented above so we point to l2 here, not l2.next
+            if not l1.next:
+                # if L1 is on the L2 chain, or if L2 is also done then we're done (L2 previously ended done or finishing at same time)
+                if single or not l2:
+                    if carry == 1:
+                        l1.next = ListNode(1)
+                    break
+                # If l2 isn't done then switch L1 to the L2 chain 
+                else:
+                    single = True
+                    l1.next = l2
+            
+            l1 = l1.next
+        
         return head
 
 
