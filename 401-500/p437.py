e3b08eb83df797f78ce2a1700be430abf6e9e36d
drewserles <drewserles@gmail.com>
2022-03-07 22:35:23 -0500

    a few array problems

--- 401-500/p437.py
+++ 401-500/p437.py
@@ -5,9 +5,30 @@ class TreeNode:
         self.left = left
         self.right = right
 
+'''
+PathSum 3: different is now that the path can start at any point
+I think this means that I need to track every possible running tally to that point
+E.g. path of 5,2,1 to a leaf. The sums would be 5 at the first spot, then [7,2] at the second, then [8,3,1]
+I'll call this a sumlist and recalculate it at each node and pass it forwards
+
+Want to return the number of possible paths so need to return a count
+Base case 1: root is Null, return 0
+Base case 2:
+'''
 class Solution:
     def pathSum(self, root, targetSum):
-        return 0
+        def dfs(root, path_sums):
+            # Base: Null root return 0
+            if not root:
+                return 0
+            # Update the existing paths to include this root's value - don't forget this node
+            path_sums = [x + root.val for x in path_sums] + [root.val]
+
+            # Current count of paths that meet the target
+            path_count = sum(x==targetSum for x in path_sums)
+
+            return path_count + dfs(root.left, path_sums) + dfs(root.right, path_sums)
+        return dfs(root, [])
 
 if __name__ == "__main__":
     sol = Solution()
--- 601-700/p658.py
+++ 601-700/p658.py
@@ -1,7 +1,8 @@
+from bisect import bisect_left
 # Find the K closest elements in arr to value x (absolute diff)
 
 class Solution:
-    def findClosestElements(self, arr, k, x):
+    def findClosestElements_v1(self, arr, k, x):
         topk = []
         for v in arr:
             res = (v, abs(v-x))
@@ -12,8 +13,35 @@ class Solution:
                     topk.append(res)
         return [x[0] for x in topk]
 
-sol = Solution()
-arr = [1,1,1,10,10,10]
-k = 1
-x = 9
-print(sol.findClosestElements(arr, k, x))
+
+
+    '''
+    Another interesting idea. Find the insertion point of x using a binary search
+        I used built in bisect here which returns the insertion index
+    Then every index > then this position has a >= diff and every index < has a <= diff.
+    Since we know it's ordered we can step these two integers out one at a time.
+    There's some complexity to keep track of like making sure they don't go outside the array bounds, and also we want the output
+    in sorted order. But since we know i wil be getting smaller and j will be getting bigger in terms of diff, you can append the j values
+    to the end of the result and insert the i value to the beginning of the result when it's their turn.
+    '''
+    def findClosestElements(self, arr, k, x):
+        j = bisect_left(arr, x)
+        i = j-1 
+        res = []
+        while 1:
+            if (j >= len(arr) and i < 0) or len(res) >= k:
+                break
+            elif j >= len(arr) or (abs(arr[i]-x) <= abs(arr[j]-x)):
+                res.insert(0, arr[i])
+                i -= 1
+            elif i < 0 or (abs(arr[i]-x) > abs(arr[j]-x)):
+                res.append(arr[j])
+                j += 1
+        return res
+
+if __name__ == "__main__":
+    sol = Solution()
+    arr = [1,1,9,10,10,10]
+    k = 3
+    x = 9
+    print(sol.findClosestElements(arr, k, x))
